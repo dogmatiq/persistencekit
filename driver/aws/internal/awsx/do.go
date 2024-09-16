9753b553aabb3cc4f4ff7a3e86f4d0383b07ba18
James Harris <contact@jamesharris.id.au>
2024-09-17 06:29:30 +1000

    Remove dynamodb specific options from `awsx.Do()`.

--- driver/aws/internal/awsx/do.go
+++ driver/aws/internal/awsx/do.go
@@ -2,24 +2,22 @@ package awsx
 
 import (
 	"context"
-
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 // Do executes an AWS API request.
 //
 // fn is a function that is called to execute the request, typically a method on
-// a *dynamodb.DynamoDB client.
+// one of the AWS client types.
 //
 // m is a function that mutates the input value before it is sent and returns
 // any options that should be used when sending the request.
-func Do[In, Out any](
+func Do[In, Out, Options any](
 	ctx context.Context,
-	fn func(context.Context, *In, ...func(*dynamodb.Options)) (Out, error),
-	m func(any) []func(*dynamodb.Options),
+	fn func(context.Context, *In, ...func(*Options)) (Out, error),
+	m func(any) []func(*Options),
 	in *In,
 ) (out Out, err error) {
-	var options []func(*dynamodb.Options)
+	var options []func(*Options)
 	if m != nil {
 		options = m(in)
 	}
